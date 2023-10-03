478c362e80165c80bb4b8a2da742cd0d8ed14747
Matias <mati10ceccato@gmail.com>
2023-10-02 21:59:17 -0300

    WebApp con WebSocket funcionando

---
+++ WebApp/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const { Server } = require("socket.io");
+const http = require("http");
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use('/socket.io', express.static(__dirname + '/node_modules/socket.io/client-dist'));
+app.set('PORT', port);
+
+// Configurar middleware para analizar JSON y URL-encoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Landing
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log("WebSocket connection established");
+});
+
+// POST route
+app.post('/data', (req, res) => {
+    const postData = req.body;
+
+    io.emit('data', postData);
+
+    res.json({ message: 'POST request received', data: postData});
+});
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
