9fb55f1144cd5f7216b0537273bb93a39e0bdaf9
Matias <mati10ceccato@gmail.com>
2023-10-02 22:12:36 -0300

    WebApp con WebSocket funcionando

--- WebApp/server.js
+++ WebApp/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-const { Server } = require("socket.io");
-const http = require("http");
+const { Server } = require('socket.io');
+const http = require('http');
 const server = http.createServer(app);
 const io = new Server(server);
 
 app.use('/socket.io', express.static(__dirname + '/node_modules/socket.io/client-dist'));
-app.set('PORT', port);
+app.set('PORT', port)
 
 // Configurar middleware para analizar JSON y URL-encoded
 app.use(express.json());
@@ -15,22 +15,21 @@ app.use(express.urlencoded({ extended: false }));
 
 // Landing
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-io.on('connection', (socket) => {
-    console.log("WebSocket connection established");
+server.on('connection', (socket) => {
+  console.log('WebSocket connection established');  
 });
 
 // POST route
 app.post('/data', (req, res) => {
-    const postData = req.body;
+  const postData = req.body;
+  io.emit('data', postData);
 
-    io.emit('data', postData);
-
-    res.json({ message: 'POST request received', data: postData});
+  res.json({ message: 'POST request received', data: postData });
 });
 
 server.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
