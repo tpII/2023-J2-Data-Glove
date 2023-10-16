c5003a204419ae1df7322cb2ad6b73e2adb95c68
Matias <mati10ceccato@gmail.com>
2023-10-16 14:13:43 -0300

    Rollback instalación de librerías

--- Firmware/include/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
-#include "stdint.h"
-#include "math.h"
-
-
-#endif /* _MAIN_H_ */
--- Firmware/src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <I2Cdev.h>
-#include <MPU6050.h>
-
-int main (void){
-  
-}
