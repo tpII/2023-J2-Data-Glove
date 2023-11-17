8d0ace0828d04d1bac6bed1f3e34b33feae13d53
Gonzalo Gil <gonzalogil123@gmail.com>
2023-11-17 11:31:56 -0300

    Data now parsed to JSON in the server

--- WebApp/server.js
+++ WebApp/server.js
@@ -27,9 +27,8 @@ server.on('connection', (socket) => {
 
 // POST route
 app.post('/data', (req, res) => {
-  const postData = req.body;
+  const postData =JSON.parse(JSON.stringify(req.body));
   io.emit('data', postData);
-  console.log(postData)
   res.json({ message: 'POST request received', data: postData });
 });
 
