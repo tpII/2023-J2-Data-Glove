9f74d2e679f805b541d3b0e79eaf721a79212e8a
Gonzalo Gil <gonzalogil123@gmail.com>
2023-11-17 11:31:18 -0300

    New card added and changes to the object scene

--- WebApp/views/main.js
+++ WebApp/views/main.js
@@ -1,15 +1,26 @@
 import * as THREE from '/three/three.module.js';
 
-var camera, scene, renderer;
+var camera, scene, scene1, scene2, renderer;
 var geometry, material, mesh, edges, line;
+var geometry2, material2, mesh2
 var container;
 var canvasWidth, canvasHeight;
+var quaternion;
 const socket = io()
 
 init();
 render();
 
+function changeScene(elem){
+    if (elem.id == "scene1") {
+        scene = scene1;
+    } else {
+        scene = scene2;
+    }
+}
+
 function init() {
+    const axesHelper = new THREE.AxesHelper(5);
     container = document.getElementById('canvas');
     document.body.appendChild(container);
     canvasWidth = container.offsetWidth;
@@ -18,19 +29,36 @@ function init() {
     camera = new THREE.PerspectiveCamera( 70, canvasWidth / canvasHeight, 0.01, 10 );
     
     // Crea la escena
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xffffff);
+    scene1 = new THREE.Scene();
+    scene1.background = new THREE.Color(0xffffff);
 
     // Crea la figura 3D y le agrega una "malla" con la textura de la figura
     geometry = new THREE.BoxGeometry( 0.3, 0.3, 0.3 );
     material = new THREE.MeshNormalMaterial();
     mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    scene1.add( mesh );
+    axesHelper.setColors(new THREE.Color('rgb(0,0,255)'), new THREE.Color('rgb(0,255,0)'), new THREE.Color('rgb(255,0,0)'));
+    scene1.add(axesHelper);
 
     // Agrega lineas blancas para delimitar los bordes de las caras del cubo
     edges = new THREE.EdgesGeometry( geometry );
     line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial( { color: 0x222021 } ) ); 
-    scene.add( line );  
+    scene1.add( line );
+    
+    // Crea la escena
+    // scene2 = new THREE.Scene();
+    // scene2.background = new THREE.Color(0xffffff);
+
+    // // Crea la figura 3D y le agrega una "malla" con la textura de la figura
+    // geometry2 = new THREE.SphereGeometry( 0.3, 0.3, 0.3 );
+    // material2 = new THREE.MeshNormalMaterial();
+    // mesh2 = new THREE.Mesh( geometry2, material2 );
+    // scene2.add( mesh2 );
+
+    // Agrega lineas blancas para delimitar los bordes de las caras del cubo
+    // edges = new THREE.EdgesGeometry( geometry );
+    // line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial( { color: 0x222021 } ) ); 
+    // scene2.add( line );
     
     // Inicializa el renderizador, habilitando el antialiasing
     renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -49,16 +77,18 @@ function init() {
     document.getElementById('cuaternionY').innerHTML = "0";
     document.getElementById('cuaternionZ').innerHTML = "0";
     document.getElementById('cuaternionW').innerHTML = "0";
+
+    scene = scene1;
 }
 
 function render(){
     socket.on('data', (data) => {
-        document.getElementById('cuaternionX').innerHTML = data.SEq1.toFixed(4);
-        document.getElementById('cuaternionY').innerHTML = data.SEq2.toFixed(4);
-        document.getElementById('cuaternionZ').innerHTML = data.SEq3.toFixed(4);
-        document.getElementById('cuaternionW').innerHTML = data.SEq4.toFixed(4);
+        // document.getElementById('cuaternionX').innerHTML = parseFloat(data.SEq_1).toFixed(4);
+        // document.getElementById('cuaternionY').innerHTML = parseFloat(data.SEq_2).toFixed(4);
+        // document.getElementById('cuaternionZ').innerHTML = parseFloat(data.SEq_3).toFixed(4);
+        // document.getElementById('cuaternionW').innerHTML = parseFloat(data.SEq_4).toFixed(4);
 
-        const quaternion = new THREE.Quaternion(data.SEq1, data.SEq2, data.SEq3, data.SEq4);
+        quaternion = new THREE.Quaternion(parseFloat(data.SEq_1), parseFloat(data.SEq_2), parseFloat(data.SEq_3), parseFloat(data.SEq_4));
         // Rota la figura un cierto angulo en radianes, determinado por los valores (x, y, z)
         mesh.rotation.setFromQuaternion(quaternion)
         line.rotation.setFromQuaternion(quaternion)
